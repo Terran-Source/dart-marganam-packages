c726a24d7395074774e092470a0e52cfffb4061f
Tamal Patra <patra.tamal@gmail.com>
2021-12-14 04:48:04 +0530

    HttpHeaderParser test: parseHeaderValue

--- test/extensions/http_response_test.dart
+++ test/extensions/http_response_test.dart
@@ -2,21 +2,25 @@ import 'package:dart_marganam/extensions/http_response.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group(
-    'HttpHeaderParser test',
-    () {
-      // TODO:
-      test('description', () {
-        // arrange
-        const headerValue =
-            'type:attachment;filename="hello.txt";filename*="hello-world.txt";utf-8'; // Content-Disposition
+  group('HttpHeaderParser test:', () {
+    test('parseHeaderValue', () {
+      // arrange
+      const headerValue = 'type:attachment;filename="hello.txt";'
+          "   spaced-key =  '   some 'spaced' value '      ; "
+          'filename*=hello-world.txt;utf-8'; // Content-Disposition
 
-        // act
-        final result = headerValue.parseHeaderValue();
-      });
-    },
-    skip: true,
-  );
+      // act
+      final result = headerValue.parseHeaderValue();
+
+      // assert
+      expect(result['type:attachment'], true);
+      expect(result['filename'], 'hello.txt');
+      expect(result['spaced-key'], "some 'spaced' value");
+      expect(result['filename*'], 'hello-world.txt');
+      expect(result['utf-8'], true);
+      expect(result['junk'], null);
+    });
+  });
 
   group(
     'HttpResponseExtension test',
