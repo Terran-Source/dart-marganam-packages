cfbf55b6e84bcbef9a8c03286431dc065dd9ea8a
Tamal Patra <patra.tamal@gmail.com>
2021-12-15 21:44:44 +0530

    EnumExt test

--- test/extensions/enum_test.dart
+++ test/extensions/enum_test.dart
@@ -2,7 +2,7 @@ import 'package:dart_marganam/extensions/enum.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('Enums for Enum.values() test:', () {
+  group('EnumTypeConverter test for Enum.values():', () {
     // TODO:
     test('toStrings', () {
       // act
@@ -24,13 +24,29 @@ void main() {
     });
   });
 
-  group(
-    'EnumExt test',
-    () {
-      // TODO:
-    },
-    skip: true,
-  );
+  _Dummy.values.forEach(testEnumExt);
+  _Days.values.forEach(testEnumExt);
+}
+
+void testEnumExt(Enum testEnum) {
+  group('EnumExt test:', () {
+    test('toEnumString', () {
+      // act
+      final result = testEnum.toEnumString();
+
+      // assert
+      expect(result, isA<String>());
+      expect(result, testEnum.name);
+    });
+    test('toEnumString(withQuote: true)', () {
+      // act
+      final result = testEnum.toEnumString(withQuote: true);
+
+      // assert
+      expect(result, isA<String>());
+      expect(result, "'${testEnum.name}'");
+    });
+  });
 }
 
 enum _Dummy { One, Two, Three }
