5dbf9cd34f727c13c23be2c4d6141489a3dd354d
Tamal Patra <patra.tamal@gmail.com>
2021-12-28 15:56:17 +0530

    added `extendDbEnumType()`

--- lib/db/enums.dart
+++ lib/db/enums.dart
@@ -4,3 +4,9 @@ enum ResourceFrom { asset, web }
 enum CustomQueryType { defaultStartup, dataInitiation, openingPragma }
 
 final Map<Type, List> enumTypes = {};
+
+void extendDbEnumType<T>(List<T> values) {
+  if (values is List<Enum> && !enumTypes.containsKey(T)) {
+    enumTypes[T] = values;
+  }
+}
