b413d0653a999cc920de6ffe9439c2c3c8ef85c2
Tamal Patra <patra.tamal@gmail.com>
2022-01-02 00:29:36 +0530

    openConnection folder renamed to open_connection

--- lib/db/drift/open_connection.dart
+++ lib/db/drift/open_connection.dart
@@ -1,8 +1,8 @@
 library marganam.db.drift.open_connection;
 
 // ! TODO: *TEST* the following platform specific export
-export 'openConnection/open_connection_stub.dart'
-    if (dart.library.html) 'openConnection/web.dart'
-    if (dart.library.io) 'openConnection/native.dart'
+export 'open_connection/open_connection_stub.dart'
+    if (dart.library.html) 'open_connection/web.dart'
+    if (dart.library.io) 'open_connection/native.dart'
     // ? doubtful if it ever reaches
-    if (dart.library.core) 'openConnection/memory.dart';
+    if (dart.library.core) 'open_connection/memory.dart';
